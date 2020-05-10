2a1ddfb51904dfcb4c1b2a4ee7ce6775c04af99e
Tomas <tomasmoiron@hotmail.com>
2020-05-10 17:20:50 -0300

    Desafio 2

--- mongo/coneccion.js
+++ mongo/coneccion.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 
 const uri = "mongodb+srv://admin:betp2@cluster0-zdy6w.mongodb.net/test?retryWrites=true&w=majority";
 const client = new mongoclient(uri, {useNewUrlParser:true, useUnifiedTopology:true});
-
+/*
 client.connect((err, result) =>{
     if(!err){
         console.log(chalk.blue('Cliente conectado'));
@@ -35,3 +35,70 @@ client.connect((err, result) =>{
         console.log(chalk.red(err));
     }
 });
+*/
+
+const inventor = {
+    first: "Martin",
+    last: "aaaaaaaa",
+    year: 1109
+}
+
+client.connect()
+    .then (function conectar(){
+        return new Promise (resolve =>{
+            console.log('BD Conectada');
+            let collectionInventors = client.db('betp2_desafio').collection('inventors');
+            resolve('Llego a conectarse')
+        })})
+
+    .then(function insertar(){
+        return new Promise (async resolve =>{
+            let collectionInventors = client.db('betp2_desafio').collection('inventors');
+            console.log('Se intenta insertar un usuario');
+            await collectionInventors.insertOne(inventor);
+            console.log('Usuario insertado');
+            resolve('Llego a insertar un inventor');
+        })})
+
+
+    .then (function encontrar(){
+        return new Promise (async function (resolve,reject) {
+            console.log('Se intenta buscar un usuario');
+            let collectionInventors = client.db('betp2_desafio').collection('inventors');
+            var user = await collectionInventors.findOne({first:"Martin"});
+            console.log('La funcion devolvio el siguiente valor: ', user);
+            if(user){
+                resolve('Se encontro el usuario');            
+            }else{
+                reject('El usuario no existe y no quiero continuar con la ejecucion');
+            }
+        })})  
+        
+        
+    .then(function modificar(){
+        return new Promise (async resolve =>{
+            console.log('Se intenta modificar un usuario');
+            let collectionInventors = client.db('betp2_desafio').collection('inventors');
+            await collectionInventors.updateOne({first: "Martin"},{$set: {first: "Perico"}});
+            console.log('Usuario modificado');
+            resolve('Llego a modificar el usuario');
+        })})
+
+
+    .then(function eliminar(){
+        return new Promise (async resolve=>{
+            console.log('Se intenta eliminar un usuario');
+            let collectionInventors = client.db('betp2_desafio').collection('inventors');
+            await collectionInventors.deleteOne({first: "Esteban"});
+            console.log('Usuario Eliminado');
+            client.close();
+            console.log('Se cerro la conexion con la BD');
+            console.log('Todas las funciones se ejecutaron con exito');
+        })})     
+
+
+    .catch(error=>{
+        console.log('OCURRIO UN ERROR: ', error);
+        console.log('Se cerro la conexion con la BD');
+        client.close();
+    })
